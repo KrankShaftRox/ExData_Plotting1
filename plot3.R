9ffb5894ddb8eecae1e5a8f2bd99b0095100689e
KrankShaftRox <64679872+KrankShaftRox@users.noreply.github.com>
2020-05-25 16:38:07 +0530

    Correct script 3

--- plot3.R
+++ plot3.R
@@ -14,8 +14,8 @@ datetime <- strptime(paste(outdat$Date, outdat$Time, sep=" "), "%d/%m/%Y %H:%M:%
 
 #Output the histogram to the correct format in png, and then close the connection
 png("plot3.png", width=480, height=480)
-plot(datetime, Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, Sub_metering_2, type="l", col="red")
-lines(datetime, Sub_metering_3, type="l", col="blue")
+plot(datetime, outdat$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
+plot(datetime, outdat$Sub_metering_2, type="l", col="red")
+plot(datetime, outdat$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
